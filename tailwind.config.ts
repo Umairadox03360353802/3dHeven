0f71ed41c8dd316d1a185e19da8ce2a2539cf9b4
Umair Mahmood <uMehmood@skillgigs.com>
2025-01-20 18:34:32 +0500

    changes-ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,12 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily:{
+        Montserrat:["Montserrat"],
+        VarelaRound:["Varela Round"],  
+
+      },
+    
     },
   },
   plugins: [],
